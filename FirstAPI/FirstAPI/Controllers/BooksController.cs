d43f7f0b2d98d30fa6b3242dfdf61f7ff7982008
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2025-08-25 20:32:02 +0530

    rest api

---
+++ FirstAPI/FirstAPI/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FirstAPI.Models;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        static private List<Book> books = new List<Book>
+        {
+            new Book { Id = 1, Title = "1984", Author = "George Orwell", YearPublished = 1949 },
+            new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", YearPublished = 1960 },
+            new Book { Id = 3, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", YearPublished = 1925 }
+        };
+    }
+}
