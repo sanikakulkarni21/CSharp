957fdbed1608cc06cd6fff89fd9ade87aa807471
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2025-09-02 23:37:15 +0530

    changes in disconnected ado

--- FirstAPI/FirstAPI/Models/Book.cs
+++ FirstAPI/FirstAPI/Models/Book.cs
@@ -3,7 +3,7 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }=null!;
+        public string Title { get; set; }=null!; //value should not be null
         public string Author { get; set; }=null!;
         public int YearPublished { get; set; }
     }
