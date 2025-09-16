4f22973ba3cca3eb06a41c9f418ce818004952e0
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2025-09-16 23:57:19 +0530

    constructor

--- sessions/c#basics/TesterApp/Program.cs
+++ sessions/c#basics/TesterApp/Program.cs
@@ -8,30 +8,101 @@ namespace TesterApp
 {
     internal class Program
     {
+
+        private Program()
+        {
+            this.programName = FirstProgram;
+        } 
+
+    
+        
+       /* public void SetProgramName(string name)//old style
+        {
+            this.programName = name;
+        }
+        public string GetProgramName()//old style
+        {
+            return this.programName;
+        }
+        public string Name //old style
+        {
+            get { return programName; }
+            set { name=value; }
+        }*/
+        public string Name1 { get; set; } //  new style auto implemented property
+
+        public Program()
+        {
+
+        }
+        public Program(string name)
+        {
+            this.programName = name;
+         }
+
+
+
         static void Main(string[] args)
         {
+
             for (int i = 0; i < args.Length; i++)
             {
                 Console.WriteLine("hello " + args[i]);
             }
-           
+
             Console.WriteLine("welcome to dotnet programming");
             int count = 45;
             count = count++;
             if (count <= 300)
             {
                 while (count < 299)
-                { 
-                Console.WriteLine("count={0}",count);
+                {
+                    Console.WriteLine("count={0}", count);
                     count++;
                 }
             }
             Console.WriteLine("enter your name");
-            
-            String name =Console.ReadLine();
+
+            String name = Console.ReadLine();
             Console.WriteLine("godmorning{0}", name);
+
+
+
+            var marks = 456;  //dynamic type
+
+            int result = Addition(23, 45);
+            int result1 = Subtraction(45, 23);
+            int result2 = Multiplication(23, 45);
+
+            Program theProgram = new Program();
+            theProgram.Display();
+
+
             Console.ReadLine();
 
         }
+
+         int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        static int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        //Member function method
+        public void Display()
+        {
+            Console.WriteLine("processing logic for display");
+            Console.WriteLine("program instance Dashboard");
+        }
+
+
     }
 }
