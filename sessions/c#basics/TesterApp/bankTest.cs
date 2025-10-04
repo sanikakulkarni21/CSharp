a429d1bc342e2edad1b260cb8be3bfb22c7c4b30
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2025-10-04 23:59:26 +0530

    changes in code

--- sessions/c#basics/TesterApp/bankTest.cs
+++ sessions/c#basics/TesterApp/bankTest.cs
@@ -12,8 +12,30 @@ namespace TesterApp
 
         static void Main(string[] args)
         {
-            Account acct = new Account(1000);
-            Console.WriteLine("Hello World!");
+            Account acct1 = new Account(56000);
+            Console.WriteLine("1. withdraw");
+            Console.WriteLine("2. deposit");
+            int option= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please Enter Amount");
+                        float amount = float.Parse(Console.ReadLine());
+
+            switch (option) {
+
+                case 1:
+                    {
+                        acct1.Withdraw(amount);
+                    }
+                    break;
+                case 2:
+                    {
+                        acct1 .Deposit(amount);
+                    } break;
+
+            }
+            float balance= acct1.Balance;
+            Console.WriteLine("your remaining Balance = {0}", balance);
+            Console.ReadLine();
         }
     }
 }
