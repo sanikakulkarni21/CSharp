6ab9fb06ce7dbbf918111c537d9b3518ac1b6cd7
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2026-06-29 15:35:56 +0530

    2Darray print logic

---
+++ Collection Framework/2Darray/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// addition of 2d array
+
+int[,]arr1 = {
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+
+int[,] arr2=
+{
+    {2,3,4},
+    {5,6,8},
+    {4,5,6}
+};
+
+int [,]result=new int[3,3];
+for(int i=0;i<3;i++)
+{
+    for(int j = 0; j <3; j++)
+    {
+        result[i,j]=arr1[i,j]+arr2[i,j];
+    }
+}
+
+for(int i = 0; i <3; i++)
+{
+    for(int j = 0; j < 3; j++)
+    {
+        Console.Write(result[i,j]+"\t ");
+    }
+    Console.WriteLine();
+}
